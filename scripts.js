b18a719d038042e981bce9f29d92d2f5f1beec90
Ruben Burgos <code.rburgos@gmail.com>
2023-05-04 09:53:38 -0500

    Wrote JS for APIs, created inital buttons to GET quotes

---
+++ scripts.js
@@ -0,0 +1,45 @@
+//Kanye-----------------------------
+const KanyeQuote = () => {
+  fetch('https://api.kanye.rest')
+  .then(res => res.json())
+  .then(data => KanyedQuote(data))
+}
+
+const KanyedQuote = (data) => {
+  const blockQuote = document
+  .getElementById('kanyed-quote');
+  blockQuote.classList.add
+  ('text-style');
+  blockQuote.innerHTML = data.quote;
+}
+//-----------------------------------
+
+//Swanson----------------------------
+const SwansonQuote = () => {
+  fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  .then(res => res.json())
+  .then(data => SwansonedQuote(data))
+}
+
+const SwansonedQuote = (data) => {
+  const blockQuote = document
+  .getElementById('swansoned-quote');
+  blockQuote.classList.add('text-style');
+  blockQuote.innerHTML = data;
+}
+//-----------------------------------
+
+
+
+
+
+
+
+
+
+/*
+const toLearn = () => {
+  const check1 = fetch('https://api.kanye.rest').then(res => res.json());
+  console.log(check1);
+}
+*/
